2e92504c58c28b7173119a8b628072912454f46c
Aleksey Tatarchuk <tatarchuk.a.s.1997@gmail.com>
2025-05-18 14:05:55 +0300

    feat: ready sun

--- src/shaders/earth/fragment.glsl
+++ src/shaders/earth/fragment.glsl
@@ -1,3 +1,10 @@
+uniform sampler2D uDayTexture;
+uniform sampler2D uNightTexture;
+uniform sampler2D uSpecularCloudsTexture;
+uniform vec3 uSunDirection;
+uniform vec3 uAtmosphereDayColor;
+uniform vec3 uAtmosphereTwilightColor;
+
 varying vec2 vUv;
 varying vec3 vNormal;
 varying vec3 vPosition;
@@ -6,7 +13,48 @@ void main()
 {
     vec3 viewDirection = normalize(vPosition - cameraPosition);
     vec3 normal = normalize(vNormal);
-    vec3 color = vec3(vUv, 1.0);
+    vec3 color = vec3(0.0);
+
+    // Sun orientation
+    float sunOrientation = dot(uSunDirection, normal);
+    // color = vec3(sunOrientation);
+
+    // Day / Night color
+    float dayMix = smoothstep(0.0, 0.6, sunOrientation);
+    vec3 dayColor = texture(uDayTexture, vUv).rgb;
+    vec3 nightColor = texture(uNightTexture, vUv).rgb;
+    color = mix(nightColor, dayColor, dayMix);
+
+    // Specular clouds
+    vec2 specularCloudsColor = texture(uSpecularCloudsTexture, vUv).rg;
+
+    // Clouds
+    float cloudsMix = smoothstep(0.7, 1.0, specularCloudsColor.g);
+    cloudsMix *= mix(0.1, 1.0, dayMix);
+    color = mix(color, vec3(1.0), cloudsMix);
+
+    // fresnel
+    float fresnel = dot(viewDirection, normal) + 1.0;
+    fresnel = pow(fresnel, 3.0);
+
+
+    // Atmosphere
+    float atmosphereDayMix = smoothstep(-0.5, 1.0, sunOrientation);
+    vec3 atmosphereColor = mix(uAtmosphereTwilightColor, uAtmosphereDayColor, atmosphereDayMix);
+    color = mix(color, atmosphereColor, fresnel * atmosphereDayMix);
+
+    // Specular
+    vec3 reflection = reflect(- uSunDirection, normal);
+    float specular = - dot(reflection, viewDirection);
+    specular = max(specular, 0.0);
+    specular = pow(specular, 32.0);
+    specular *= specularCloudsColor.r;
+
+    vec3 specularColor = mix(vec3(1.0), atmosphereColor, fresnel);
+    color += specular * specularColor;
+
+    // color = vec3(specularCloudsColor.r);
+
 
     // Final color
     gl_FragColor = vec4(color, 1.0);
